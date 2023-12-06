df16a335554aebbea99fbf560032fa9629d543f1
Yo're-In_David <51287631+Mad-Engineer-142@users.noreply.github.com>
2023-12-06 12:23:55 +0300

    Create app[ajsdhbfouqwyebfo83yb484t87g4of8qger9q3p498thp3948hq934gqboewr9ugq349g].py
    ajsdhbfouqwyebfo83yb484t87g4of8qger9q3p498thp3948hq934gqboewr9ugq349g

---
+++ app[ajsdhbfouqwyebfo83yb484t87g4of8qger9q3p498thp3948hq934gqboewr9ugq349g].py
@@ -0,0 +1,10 @@
+ENtdQdOu = 7
+DXDxjAvF = 1
+if ENtdQdOu < 0:
+    print('Sorry, factorial does not exist for negative numbers')
+elif ENtdQdOu == 0:
+    print('The factorial of 0 is 1')
+else:
+    for ahYugBwZ in range(1, ENtdQdOu + 1):
+        DXDxjAvF = DXDxjAvF * ahYugBwZ
+    print('The factorial of', ENtdQdOu, 'is', DXDxjAvF)
